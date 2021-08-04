2bf861619e3efdcaf90018729e654a672247efbf
Austin Whitley <whitley.austin01@gmail.com>
2021-08-04 12:02:05 -0500

    everything up to date

---
+++ src/main/java/com/outfitterexpert/outfitterexpert/OutfitterexpertApplication.java
@@ -0,0 +1,13 @@
+package com.outfitterexpert.outfitterexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutfitterexpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutfitterexpertApplication.class, args);
+    }
+
+}
