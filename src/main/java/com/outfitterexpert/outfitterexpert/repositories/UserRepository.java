8cf48a06dd779db43c7ad5dd553dd401fa2e4eb6
Irvin Buendia <irvin.buendia1@gmail.com>
2021-08-06 14:01:49 -0500

    fixed issues with login' ;

--- src/main/java/com/outfitterexpert/outfitterexpert/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.outfitterexpert.outfitterexpert.repositories;
-
-import org.apache.catalina.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findById(long id);
-    User findByUsername(String username);
-}
