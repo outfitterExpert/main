4213f517ba826808b57006202a62eb36e2aca0d7
Austin Whitley <whitley.austin01@gmail.com>
2021-08-09 13:15:11 -0500

    updated table to hold desc, lodging, guided

--- src/main/java/com/outfitterexpert/outfitterexpert/SecurityConfiguration.java
+++ src/main/java/com/outfitterexpert/outfitterexpert/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/ads") // user's home page, it can be any URL
+                .defaultSuccessUrl("/") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 /* Logout configuration */
                 .and()
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/ads", "/posts") // anyone can see the home and the ads pages
+                .antMatchers("/", "/listings") // anyone can see the home and the ads pages
                 .permitAll()
                 /* Pages that require authentication */
                 .and()
