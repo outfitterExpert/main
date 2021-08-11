2ba83bef3e81247307148631877aec33595877fe
Naysa Moreno <naysaymoreno@gmail.com>
2021-08-11 12:08:49 -0500

    fixed minor bugs in listingController still need to add to package form html page

--- src/main/java/com/outfitterexpert/outfitterexpert/controllers/ListingController.java
+++ src/main/java/com/outfitterexpert/outfitterexpert/controllers/ListingController.java
@@ -1,8 +1,10 @@
 package com.outfitterexpert.outfitterexpert.controllers;
 
 
+import com.outfitterexpert.outfitterexpert.models.ListingPackage;
 import com.outfitterexpert.outfitterexpert.models.Property;
 import com.outfitterexpert.outfitterexpert.models.User;
+import com.outfitterexpert.outfitterexpert.repositories.PackageRepository;
 import com.outfitterexpert.outfitterexpert.repositories.PropertyRepository;
 import com.outfitterexpert.outfitterexpert.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ListingController {
     private final PropertyRepository propertyDao;
     private final UserRepository userDao;
+    private final PackageRepository packageDao;
 
 
-    public ListingController(PropertyRepository propDao, UserRepository userDao) {
+    public ListingController(PropertyRepository propDao, UserRepository userDao, PackageRepository packageDao) {
         this.propertyDao = propDao;
         this.userDao = userDao;
+        this.packageDao = packageDao;
     }
 
 
@@ -60,34 +64,31 @@ public class ListingController {
     @GetMapping("/listings")
     public String viewPackages(Model model) {
         model.addAttribute("package", propertyDao.findAll());
-        return "listings/packages";
+        return "listings/index";
     }
     @GetMapping("/listings/package/create")
     public String createPackage(Model model){
             User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             if(currentUser != null){
-                model.addAttribute("package", new Property());
-                return "listings/package/create";
+                model.addAttribute("listPackage", new ListingPackage());
+                return "listings/createPackage";
             }
-            return "redirect:/listings/package";
+            return "redirect:/listings/index";
     }
 
     @PostMapping("/listings/package/create")
-    public String submitPackage(@ModelAttribute Property p){
-//        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User u = userDao.findById(1L);
+    public String submitPackage(@ModelAttribute ListingPackage listPackage) {
+        Property property = propertyDao.findById(1L);
+        listPackage.setProperty(property);
+        packageDao.save(listPackage);
 
-        p.setUser(u);
-        propertyDao.save(p);
-
-        System.out.println(p.getProperty_id());
-        System.out.println(p.getDescription());
-        System.out.println(p.getDuration());
-        System.out.println(p.isGuided());
-        System.out.println(p.getUser().getId());
+        System.out.println(listPackage.getProperty_id());
+        System.out.println(listPackage.getDescription());
+        System.out.println(listPackage.getDuration());
+        System.out.println(listPackage.isGuided());
 
         return "redirect:/listings/package";
-
+    }
 
 //    @PostMapping("/listing/create")
 //    public String postNewListing(){
--- src/main/java/com/outfitterexpert/outfitterexpert/repositories/PackageRepository.java
+++ src/main/java/com/outfitterexpert/outfitterexpert/repositories/PackageRepository.java
@@ -1,2 +1,8 @@
-package com.outfitterexpert.outfitterexpert.repositories;public interface PackageRepository {
+package com.outfitterexpert.outfitterexpert.repositories;
+import com.outfitterexpert.outfitterexpert.models.ListingPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+public interface PackageRepository extends JpaRepository<ListingPackage, Long> {
+    ListingPackage findById(long id);
+    ListingPackage save(ListingPackage p);
 }
