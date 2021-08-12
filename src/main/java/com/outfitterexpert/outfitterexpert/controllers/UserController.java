48dedd996d156f67ed452c48a63e88b4135d2406
Austin Whitley <whitley.austin01@gmail.com>
2021-08-12 15:43:23 -0500

    added a user profile page

--- src/main/java/com/outfitterexpert/outfitterexpert/controllers/UserController.java
+++ src/main/java/com/outfitterexpert/outfitterexpert/controllers/UserController.java
@@ -1,22 +1,29 @@
 package com.outfitterexpert.outfitterexpert.controllers;
 
 import com.outfitterexpert.outfitterexpert.models.User;
+import com.outfitterexpert.outfitterexpert.repositories.PropertyRepository;
 import com.outfitterexpert.outfitterexpert.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.awt.*;
 
 @Controller
 public class UserController {
-    private UserRepository users;
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userDao;
+    private final PasswordEncoder passwordEncoder;
+    private final PropertyRepository propertyDao;
 
-    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
-        this.users = users;
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder, PropertyRepository propertyDao) {
+        this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
+        this.propertyDao = propertyDao;
     }
 
     @GetMapping("/sign-up")
@@ -29,7 +36,26 @@ public class UserController {
     public String saveUser(@ModelAttribute User user){
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
-        users.save(user);
+        userDao.save(user);
         return "redirect:/login";
     }
+
+    @GetMapping("/profile/{id}")
+    public String viewProfile(@PathVariable long id, Model model){
+        User user = userDao.findById(id);
+
+        model.addAttribute("user", user);
+        model.addAttribute("listings", propertyDao.findByUserId(id));
+
+        //check to see if the current user has the same id as the account
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(currentUser.getId() == id){
+            model.addAttribute("isUserAccount", true);
+        }else if(currentUser.getId() != id){
+            model.addAttribute("isUserAccount", false);
+        }
+        return "users/profile";
+    }
+
+
 }
