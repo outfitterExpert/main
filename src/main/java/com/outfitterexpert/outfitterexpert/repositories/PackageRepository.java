dfded9f13d07852d959999996a33228c3c11bdfe
Naysa Moreno <naysaymoreno@gmail.com>
2021-08-16 22:39:17 -0500

    fixed the create form for listing, still need to fix date format for package form to enter in the database

--- src/main/java/com/outfitterexpert/outfitterexpert/repositories/PackageRepository.java
+++ src/main/java/com/outfitterexpert/outfitterexpert/repositories/PackageRepository.java
@@ -1,8 +1,15 @@
 package com.outfitterexpert.outfitterexpert.repositories;
 import com.outfitterexpert.outfitterexpert.models.ListingPackage;
+import com.outfitterexpert.outfitterexpert.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 public interface PackageRepository extends JpaRepository<ListingPackage, Long> {
     ListingPackage findById(long id);
     ListingPackage save(ListingPackage p);
+
+    @Query("FROM ListingPackage p where p.property.id = ?1")
+    List<ListingPackage> findByPropertyId(long id);
 }
