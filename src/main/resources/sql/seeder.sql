d84ca42d885f32e37fd12447f2bfe005305f10dc
Naysa Moreno <naysaymoreno@gmail.com>
2021-08-19 16:41:49 -0500

    added to the seeder file and changed script to be in the create page only

--- src/main/resources/sql/seeder.sql
+++ src/main/resources/sql/seeder.sql
@@ -8,11 +8,11 @@ VALUES  ('austin_user', '$2a$10$UJdkeUuxv/ijtvR6K0eAuehnV363r7f6HT.VZ3eHmXhhGKYh
         ('irvin_user', '$2a$10$UJdkeUuxv/ijtvR6K0eAuehnV363r7f6HT.VZ3eHmXhhGKYhtALRK', 'Irvin', 'Buendia', 'admin1@email.com', true),
         ('benny_user', '$2a$10$UJdkeUuxv/ijtvR6K0eAuehnV363r7f6HT.VZ3eHmXhhGKYhtALRK', 'Benny', 'Alvarez', 'admin1@email.com', true);
 
-INSERT INTO properties(title, location, acres, slots, guided, lodging, method, owner_id)
-VALUES ('West-end King ranch', 'South Texas', 100000, 125, true, true, 'rifle', 1),
-       ('Texas Exotic Hunts', 'Texas', 798, 1, true, false, 'rifle', 3),
-       ('Waterfowl package - all inclusive', 'Manitoba, Canada', 345, 1, true, false, 'shotgun', 2),
-       ('Late Season Bow Whitetail Hunt', 'British Columbia, Canada', 3763, 6, true, true, 'bow', 4);
+INSERT INTO properties(title, location, acres, slots, guided, lodging, method, owner_id, img_url)
+VALUES ('West-end King ranch', 'South Texas', 100000, 125, true, true, 'rifle', 1, 'https://cdn.filestackcontent.com/Ktwfy5keSHaze3YmzsiJ'),
+       ('Texas Exotic Hunts', 'Texas', 798, 1, true, false, 'rifle', 3, 'https://cdn.filestackcontent.com/Ktwfy5keSHaze3YmzsiJ'),
+       ('Waterfowl package - all inclusive', 'Manitoba, Canada', 345, 1, true, false, 'shotgun', 2, 'https://cdn.filestackcontent.com/Ktwfy5keSHaze3YmzsiJ'),
+       ('Late Season Bow Whitetail Hunt', 'British Columbia, Canada', 3763, 6, true, true, 'bow', 4, 'https://cdn.filestackcontent.com/Ktwfy5keSHaze3YmzsiJ');
 
 INSERT INTO animals (id, name)
 VALUES (1, 'Bear'),
